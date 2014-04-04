dac5530bdceac4b2b7339fdf88d4a660f4bb18c4
Andre Tebart <mortydeluexe@googlemail.com>
2014-04-04 13:17:52 +0200

    Learning resource widget now shows descriptions for learning indicators

--- henm1314g3/js/tree.js
+++ henm1314g3/js/tree.js
@@ -1,16 +1,16 @@
 $.prototype.tree = function(){
 	var tree = $(this);
-	tree.find('li:has(ul)').addClass('parent_li').find(' > span').append("<span class='glyphicon glyphicon-collapse-down pull-right'></span>")
+	tree.find('li:has(ul)').addClass('parent_li').find(' > span p:first-child').append("<span class='glyphicon glyphicon-collapse-down pull-right'></span>")
 	tree.find('li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
 	tree.find('li.parent_li > ul > li').hide();
 	tree.find('li.parent_li > span').on('click', function (e) {
 	    var children = $(this).parent('li.parent_li').find(' > ul > li');
 	    if (children.is(":visible")) {
 	        children.hide('fast');
-	        $(this).find(">.glyphicon-collapse-up").removeClass("glyphicon-collapse-up").addClass("glyphicon-collapse-down");
+	        $(this).find("p:first-child .glyphicon-collapse-up").removeClass("glyphicon-collapse-up").addClass("glyphicon-collapse-down");
 	    } else {
 	        children.show('fast');
-	        $(this).find(">.glyphicon-collapse-down").removeClass("glyphicon-collapse-down").addClass("glyphicon-collapse-up");
+	        $(this).find("p:first-child .glyphicon-collapse-down").removeClass("glyphicon-collapse-down").addClass("glyphicon-collapse-up");
 	    }
 	    e.stopPropagation();
 	    });
