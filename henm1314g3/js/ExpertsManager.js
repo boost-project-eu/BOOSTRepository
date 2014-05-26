92abb78092f84cebb05bbd04e7ab1e95a8d5e7e7
Kateryna <neulinger@dbis.rwth-aachen.de>
2014-05-26 11:06:26 +0200

    Added new stuff from Ehsan

---
+++ henm1314g3/js/ExpertsManager.js
@@ -0,0 +1,81 @@
+function Expert(object){
+	if(object.hasOwnProperty("uri"))
+		this.uri = object.uri;
+	else
+		this.uri = "";
+
+	if(object.hasOwnProperty("bcnUri"))
+		this.bcnUri = object.bcnUri;
+	else
+		this.bcnUri = "";
+
+	if(object.hasOwnProperty("liId"))
+		this.liId = object.liId;
+	else
+		this.liId = "";
+
+	if(object.hasOwnProperty("name"))
+		this.name = object.name;
+	else
+		this.name = "";
+
+	if(object.hasOwnProperty("email"))
+		this.email = object.email;
+	else
+		this.email = "";
+
+	if(object.hasOwnProperty("tel"))
+		this.tel = object.tel;
+	else
+		this.tel = "";
+
+	if(object.hasOwnProperty("address"))
+		this.address = object.address;
+	else
+		this.address = "";
+
+	if(object.hasOwnProperty("website"))
+		this.website = object.website;
+	else
+		this.website = "";
+
+	if(object.hasOwnProperty("skype"))
+		this.skype = object.skype;
+	else
+		this.skype = "";
+
+}
+
+Expert.prototype.create = function(callback){
+	var space = new openapp.oo.Resource(openapp.param.space());
+	var thisExpert = this;
+	space.create({
+		relation: openapp.ns.role + "data",
+		type: "my:ns:Expert",
+		representation: this, //The representation refers to the object
+		callback: function(ExpertResource){
+			//Now we have an URI for our Expert and we need to update the resource
+			thisExpert.uri = ExpertResource.getURI();
+			ExpertResource.setRepresentation(thisExpert, "application/json", function(){
+				callback();
+			});
+		}
+	});
+}
+
+Expert.prototype.update = function(callback){
+	var ExpertResource = new openapp.oo.Resource(this.uri);
+	ExpertResource.setRepresentation(this, "application/json", function(){
+		callback();
+	});
+}
+
+Expert.prototype.delete = function(callback){
+	openapp.resource.del(this.uri, function(){
+		callback();
+	});
+}
+
+function retrieveAllExperts(callback){
+	retrieveBoostResources("my:ns:Expert", function(object){return new Expert(object)}, callback);
+}
