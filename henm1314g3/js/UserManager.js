4509969047e08ed7e32a8e1dde439f02d310fb94
peetwriter <pereter92@gmail.com>
2014-07-15 17:42:09 +0200

    first part of require widgets

--- henm1314g3/js/UserManager.js
+++ henm1314g3/js/UserManager.js
@@ -40,54 +40,56 @@ User.prototype.saveAccessRights = function(callback){
 
 function retrieveAllUsers(space, callback){
 	//Get the owner of the space
-	space.getSubResources({
-		relation : "http://purl.org/openapp/owner",
-		onAll : function(ownerResource){
-			var ownerUri = ownerResource[0].info.subject["http://www.w3.org/2002/07/owl#sameAs"][0].value;
+	if (space){
+		space.getSubResources({
+			relation : "http://purl.org/openapp/owner",
+			onAll : function(ownerResource){
+				var ownerUri = ownerResource[0].info.subject["http://www.w3.org/2002/07/owl#sameAs"][0].value;
+			
+
+
 		
+				//First get the URIs of all the user which are in the space
+				space.getSubResources({
+					relation : "http://xmlns.com/foaf/0.1/member",
+					type : "http://purl.org/role/terms/Person",
+					followReference : true,
+					onAll : function(resources){
+						var userUris = [];
 
+						for(var i = 0; i < resources.length; i++){
+							userUris.push(resources[i].info.subject["http://www.w3.org/2002/07/owl#sameAs"][0].value);
+						}
+						//Get all the users in the space based on their URI
+						var users = [];
 
-	
-			//First get the URIs of all the user which are in the space
-			space.getSubResources({
-				relation : "http://xmlns.com/foaf/0.1/member",
-				type : "http://purl.org/role/terms/Person",
-				followReference : true,
-				onAll : function(resources){
-					var userUris = [];
+						var getUser = function(item, done){
+							var userResource = new openapp.oo.Resource(item);
+							userResource.getInfo(function(info){
+								var email = info["http://xmlns.com/foaf/0.1/mbox"];
+								if(!email)
+									email = "";
+								//Remove 'mailto:' prefix from the email
+								email = email.substring(7);
+								var isOwner = (ownerUri == item);
+								var user = new User({
+									uri : item,
+									name : info["http://purl.org/dc/terms/title"],
+									email : email,
+									isOwner : isOwner
+								});
 
-					for(var i = 0; i < resources.length; i++){
-						userUris.push(resources[i].info.subject["http://www.w3.org/2002/07/owl#sameAs"][0].value);
-					}
-					//Get all the users in the space based on their URI
-					var users = [];
-
-					var getUser = function(item, done){
-						var userResource = new openapp.oo.Resource(item);
-						userResource.getInfo(function(info){
-							var email = info["http://xmlns.com/foaf/0.1/mbox"];
-							if(!email)
-								email = "";
-							//Remove 'mailto:' prefix from the email
-							email = email.substring(7);
-							var isOwner = (ownerUri == item);
-							var user = new User({
-								uri : item,
-								name : info["http://purl.org/dc/terms/title"],
-								email : email,
-								isOwner : isOwner
+								users.push(user);
+								done();
 							});
+						}
 
-							users.push(user);
-							done();
+						async.forEach(userUris, getUser, function(error){
+							callback(users);
 						});
 					}
-
-					async.forEach(userUris, getUser, function(error){
-						callback(users);
-					});
-				}
-			});
-		}
-	});
+				});
+			}
+		});
+	}
 }
--- henm1314g3/js/config.js
+++ henm1314g3/js/config.js
@@ -1,20 +1,28 @@
 require.config({
     paths: {
-        jquery: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/jquery-1.10.2.min.js",
-        jqueryUi: "//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js",
-        bootstrap: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/bootstrap.min.js",
-        bootShared: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/boostShared.js",
-        tree : "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/tree.js",
-        spin : "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/ladda/spin.min.js",
-        ladda : "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/ladda/ladda.min.js",
-        ractive : "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/Ractive.js",
-        async: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/async.js",
-
-        BCNManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/BCNManager.js",
-        LearningDocumentsManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/LearningDocumentsManager.js",
-        ExpertsManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/ExpertsManager.js",
-        WidgetsManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/WidgetsManager.js",
-        AccessRightsManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/AccessRightsManager.js",
-        iwc : "http://dbis.rwth-aachen.de/gadgets/iwc/lib/iwc.js"        
+        jquery: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/jquery-1.10.2.min",
+        jqueryUi: "//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min",
+        async: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/async",
+        bootstrap: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/bootstrap.min",
+        bootShared: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/boostShared",
+        bootstrapSlider: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/bootstrap-slider",
+        tree: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/tree",
+        spin: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/ladda/spin.min",
+        ladda: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/ladda/ladda.min",
+        ractive: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/Ractive",
+        bootbox: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/bootbox.min",
+        rivets: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/rivets.min",
+        repositories: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/repositories",
+        
+        UserManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/UserManager",       
+        BCNManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/BCNManager",
+        LearningDocumentsManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/LearningDocumentsManager",
+        ExpertsManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/ExpertsManager",
+        EmployeeManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/EmployeeManager",
+        WidgetsManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/WidgetsManager",
+        AccessRightsManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/AccessRightsManager",
+        ConfigManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/ConfigManager",
+        iwc : "http://dbis.rwth-aachen.de/gadgets/iwc/lib/iwc"        
     }
+
 });
