70a209943fa29d8fd934dc33718fefa8ec38233a
peetwriter <pereter92@gmail.com>
2014-07-15 15:04:04 +0200

    config with paths

---
+++ henm1314g3/js/config.js
@@ -0,0 +1,20 @@
+require.config({
+    paths: {
+        jquery: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/jquery-1.10.2.min.js",
+        jqueryUi: "//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js",
+        bootstrap: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/bootstrap.min.js",
+        bootShared: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/boostShared.js",
+        tree : "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/tree.js",
+        spin : "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/ladda/spin.min.js",
+        ladda : "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/ladda/ladda.min.js",
+        ractive : "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/Ractive.js",
+        async: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/async.js",
+
+        BCNManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/BCNManager.js",
+        LearningDocumentsManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/LearningDocumentsManager.js",
+        ExpertsManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/ExpertsManager.js",
+        WidgetsManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/WidgetsManager.js",
+        AccessRightsManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/AccessRightsManager.js",
+        iwc : "http://dbis.rwth-aachen.de/gadgets/iwc/lib/iwc.js"        
+    }
+});
