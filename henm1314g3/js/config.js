7242cf091d5da0ce56d99b35fefe681bf8780e47
peetwriter <pereter92@gmail.com>
2014-07-24 10:49:16 +0200

    uitils added to some widgets

--- henm1314g3/js/config.js
+++ henm1314g3/js/config.js
@@ -30,7 +30,8 @@ require.config({
         AccessRightsManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/AccessRightsManager",
         ConfigManager: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/ConfigManager",
         iwc : "http://dbis.rwth-aachen.de/gadgets/iwc/lib/iwc",
-        google_api: "https://apis.google.com/js/client"      
+        google_api: "https://apis.google.com/js/client",
+        utils: "http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/js/utils"
     },
     shim: {
         async : {
