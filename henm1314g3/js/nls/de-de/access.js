9b6c9bd98c00321c95c666491a0fa73e4b0bc248
peter <pereter92@gmail.com>
2014-12-02 21:49:37 +0200

    Translation structure for access rights widget finalized

--- henm1314g3/js/nls/de-de/access.js
+++ henm1314g3/js/nls/de-de/access.js
@@ -1,3 +1,27 @@
 define({
-    "header" : "Guten Tag"
+    "STR_ACCESS_RIGHTS": "Deuch Access Rights",
+    "STR_MANAGE_ACCESS_RIGHTS_OF_USERS": "Manage the access rights of all the different users.",
+    "STR_TEAM_BOOST": "Team Boost",
+    "STR_PLEASE_WAIT": "Please wait.",
+    "STR_GOALS_HAVE_CHANGED": "Goals have changed.",
+    "STR_YOU_HAVE_THE_ROLE_EMPLOYEE_YOU": "You have the role <strong>Employee</strong>. You can not change anything in this widget",
+    "STR_YOU_HAVE_NO_ROLE": "You have <strong>NO</strong> roles now. Please, contact Manager to assign roles for you",
+    "STR_YOU_HAVE_ROLES_MANAGER_AND_EMPLOYEE": "You have both <strong>Manager</strong> and <strong>Employee</strong> roles.",
+    "STR_YOU_HAVE_THE_ROLE_MANAGER": "You have the role <strong>Manager</strong>.",
+    "STR_TO_SEE_THE_SPACE_AS_EMPLOYEE": "To see the space as <strong>Employee</strong> or <strong>Manager</strong>, just tick or untick Employee checkbox.",
+    "STR_NAME": "Name",
+    "STR_MANAGER": "Manager",
+    "STR_EMPLOYEE": "Employee",
+    "STR_USER_NEEDS_TO_AGREE_TERMS": "This user needs to agree to the terms of use first.",
+    "STR_THIS_IS_THE_OWNER_OF_THE_SPACE": "This is the owner of the space.",
+    "STR_ACCESS_RIGHTS_HELP": "Access Rights Help",
+    "STR_IN_THIS_WIDGET_MANAGER_CAN_DIFFERENTIATE": "In this widget manager can differentiate access to the BOOST widgets. Employees do not have access to the Access Rights widget.",
+    "STR_TO_ASSING_A_MANAGER_OR_AN_EMPLOYEE": "To assing a manager or an employee user role, tick the appropriate checkbox <input type=\"checkbox\">. If you are a manager and would like to see widgets interface for the employee role, tick both checkboxes.",
+    "STR_EMPLOYEE_COLONS": "Employee: ",
+    "STR_CAN_SEARCH_THROUGH_THE_LEARNING": "Can search through the learning repositories. Add learning resources, experts and widgets to the system. Watch own learning progress and the progress of the goals.",
+    "STR_MANAGER_COLONS": "Manager: ",
+    "STR_HAS_FULL_CONTROL_OVER_THE_SYSTEM": "Has full control over the system. Can assing access rights to the users. Can add/edit/delete employees and assign learning levels to them. Can specify deadlines for the goals and learning indicators. Can add/edit/delete business goals and learning indicators. Watch all employees progress/overall progress/progress over learning goals/progress over learning indicators.",
+    "STR_USER_OWNER_OF_SPACE": "This user is the owner of the space.",
+    "STR_THIS_USER_HAS_NOT_AGREED_TO_THE": "This user has not agreed to the terms of use yet.",
+    "STR_CLOSE": "Close"
 });
