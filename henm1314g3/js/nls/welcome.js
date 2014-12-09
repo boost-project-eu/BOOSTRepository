109e822c7be3409bc174e5253fee03a031fab083
peter <pereter92@gmail.com>
2014-12-09 22:22:46 +0200

    Welcome widget translations, change language buttons

--- henm1314g3/js/nls/welcome.js
+++ henm1314g3/js/nls/welcome.js
@@ -1,6 +1,22 @@
 define({
     "root" : {
-        "header" : "I speak English"
+        "STR_WELCOME": "Welcome",
+        "STR_WECOME_WIDGET": "Wecome Widget.",
+        "STR_WELCOME_TO_THE_BOOST_PROJECT": "Welcome to the BOOST Project",
+        "STR_BOOST_STANDS_FOR_BUSINESS_PERFORMANCE": "<small><strong>BOOST</strong> stands for  <strong>B</strong>usiness perf<strong>O</strong>rmance impr<strong>O</strong>vement through individual employee <strong>S</strong>kills <strong>T</strong>raining.</small>",
+        "STR_WITH_BOOST_YOU_CAN_ORGANISE_MANAGE": "<small>With BOOST you can organise, manage and monitor training process in the small and micro enterprises. Below are a few steps to help you get started.</small>",
+        "STR_IF_YOU_ARE_A_MANAGER": "<small><strong>If you are a Manager:</strong></small>",
+        "STR_ASSIGN_USER_ROLES_TO_EMPLOYEES": "<li><small>Assign user roles to employees using the <strong>Access Rights</strong> widget.</small></li><li><small>Define business goals and add learning indicators to them using the <strong>Business Goals</strong> widget. Assign priority to the goals, specifying the most improtant with a high priority.</small></li><li><small>Monitor business goals progress and employees competence in the <strong>Learning Progress</strong> widget. It works together with <strong>Business Goals</strong> and <strong>Business Employees</strong> widgets.</small> </li><li><small>Edit information about employees and assign goals to them, adjust their learning levels in the <strong>Business Employees</strong> widget.</small></li>",
+        "STR_IF_YOU_ARE_AN_EMPLOYEE": "<small><strong>If you are an Employee:</strong></small>",
+        "STR_USE_A_BOOST_SEARCH_WIDGET_TO": "<li><small>Use a <strong>Boost Search</strong> widget to ﬁnd learning materials.</small></li> <li><small>Watch and read interesting resources using <strong>Boost Viewer</strong> widget.</small></li><li><small>Save and organise your learning resources in the <strong>Learning Repository:</strong> You can review and add new learning documents, experts and widgets assigned to the goal and learning indicators.</small></li><li><small>Monitor your learning progress with the <strong>Learning Progress</strong> widget.</small></li>",
+        "STR_WE_HOPE_THIS_QUICK_START_HAS": "<small>We hope this Quick Start has been helpful in showing you how to get started using BOOST quickly and easily. For more detailed information please see help information provided in each widget and the BOOST User Guide, which is accessible <a href=\"http://127.0.0.1:8073/role/BOOSTRepository/henm1314g3/resources/boostGuide.pdf\" target=\"_blank\">here</a>.</small>",
+        "STR_LICENSE_AGREEMENT": "License agreement",
+        "STR_THIS_PROJECT_HAS_BEEN_FUNDED": "This project has been funded with support from the European Commission.This web site reflects the views only of the authors, and the Commission cannot be held responsible for any use which may be made of the information contained therein.",
+        "STR_AGREE": "Agree",
+        "STR_SORRY_THE_LICENSE_FOR_BOOST_WAS": "Sorry, the license for BOOST was not accepted",
+        "STR_BEFORE_PROCEEDING_YOU_MUST_FIRST": "Before proceeding you must first launch BOOST Welcome Widget and accept the End User License Agreement",
+        "STR_SORRY_YOU_DO_NOT_HAVE_RIGHTS": "Sorry, you do not have rights to access this widget",
+        "STR_BEFORE_PROCEEDING_PLEASE_CONTACT": "Before proceeding please contact your manager or administrator"
     },
     "de-de" : true
 });
